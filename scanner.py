44bdbf00de679de25c7aa36e68dda9767d66ffae
Ryan Byrne <rbyrne01@live.com>
2020-10-13 18:41:44 -0500

    version1

--- scanner.py
+++ scanner.py
@@ -5,9 +5,9 @@ import pandas as pd
 pdfFileObj = open('ISTest.pdf','rb')
 pdfReader = pdf.PdfFileReader(pdfFileObj)
 numpages = pdfReader.numPages
-text = ''
+text = []
 for page in range(numpages):
     pageObj = pdfReader.getPage(page)
-    text = text + pageObj.extractText()
+    text = text.append(pageObj.extractText().split('\n'))
 print(text)
 
