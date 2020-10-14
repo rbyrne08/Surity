a6f50bd14fae4f84fb61b369b76dae713771e31b
Ryan Byrne <rbyrne01@live.com>
2020-10-13 20:08:36 -0500

    version3

--- scanner.py
+++ scanner.py
@@ -11,6 +11,33 @@ for page in range(numpages):
     obj = pageObj.extractText().split('\n')
     for a in obj:
         text.append(a)
-for i in range(len(text)):
-    print(text[i])
 
+data = parseLine(text)
+print(data)
+
+
+#Read lines in the array, create dataframe with keywords, and amount columns
+def parseLine(textList):
+    data = pd.DataFrame(columns = ['index','keywords','amount'])
+    for line in range(len(textList)):
+        temp = textList[line].strip()
+        if (temp != ''):
+            data = data.append(pd.DataFrame({"index":[len(data)],
+                                      "keywords":[temp],
+                                      "amount":[parseAmount(temp)]
+                                      }))
+    return data
+
+#return amount in a string
+def parseAmount(lineItem):
+    digit = ''
+    for i in lineItem:
+        if i.isdigit():
+            digit = digit+ i
+    try:
+        digit = float(digit)
+    except:
+        print("no digits")
+    if(lineItem.find('(')!= -1 and lineItem[lineItem.find('(')+1].isdigit()):
+        digit *= -1
+    return digit
